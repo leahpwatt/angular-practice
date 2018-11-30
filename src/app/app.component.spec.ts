c58b0fa19b58ae4f189ca991da41ff4c9b90143a
Watt <v-lwatt@c02tj232gtdy.mi.corp.rockfin.com>
2018-11-30 08:31:47 -0500

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-proj'`, () => {
+  it(`should have as title 'proj'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-proj');
+    expect(app.title).toEqual('proj');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-proj!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to proj!');
   });
 });
